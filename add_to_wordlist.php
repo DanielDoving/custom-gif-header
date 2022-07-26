20fba827589de48c586cb27b30bb517efe64eaad
Daniel Doving <d.doving@glarotech.ch>
2022-07-26 15:57:26 +0200

    added wordlist manager

--- add_to_wordlist.php
+++ add_to_wordlist.php
@@ -1,61 +1,16 @@
 <?php
 require __DIR__ . '/config.inc.php';
-
-if (!file_exists(WORDLIST_FILE)) {
-    file_put_contents(WORDLIST_FILE, json_encode([], JSON_PRETTY_PRINT));
-}
-
-$wordlist = json_decode(file_get_contents(WORDLIST_FILE), true);
+require __DIR__ . '/classes/wordlist_manager.def.php';
 
 if (!isset($argv[1])) {
     echo "No word provided" . PHP_EOL;
     exit(1);
 }
-$default_limit = 30;
 $word_to_add   = $argv[1];
-$limit         = $argv[2] ?? $default_limit;
-
-if ($limit > 50) {
-    echo "Max limit is 50!";
-    exit(1);
-}
-
-// Check if word already exists
-$word_found = false;
-$action     = "No action needed";
-
-foreach ($wordlist as $k => $word) {
-    if ($word === $word_to_add) {
-        $word_found = true;
-        if ($limit !== $default_limit) {
-            $action       = "Updated limit '$word_to_add' in wordlist (new limit: '$limit')" . PHP_EOL;
-            $wordlist[$k] = [
-                'keyword' => $word_to_add,
-                'limit'   => $limit
-            ];
-        }
-    }
-    else if (isset($word['keyword']) && $word['keyword'] === $word_to_add) {
-        $word_found = true;
-        if ($word['limit'] !== $limit) {
-            $action       = "Updated limit '$word_to_add' in wordlist (new limit: '$limit')" . PHP_EOL;
-            $wordlist[$k] = [
-                'keyword' => $word_to_add,
-                'limit'   => $limit
-            ];
-        }
-    }
-}
-if (!$word_found) {
-    $action     = "Added word '$word_to_add' to wordlist (new limit: '$limit')" . PHP_EOL;
-    $wordlist[] = [
-        'keyword' => $word_to_add,
-        'limit'   => $limit
-    ];
-}
-file_put_contents(WORDLIST_FILE, json_encode($wordlist, JSON_PRETTY_PRINT));
-echo $action;
-exit(0);
+$limit         = $argv[2] ?? DEFAULT_LIMIT;
+$wordlist_manager = new \wordlist_manager();
+$wordlist_manager->add_or_update($word_to_add, $limit);
+exit();
 
 
 
---
+++ classes/wordlist_manager.def.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once dirname(__DIR__) . '/config.inc.php';
+
+class wordlist_manager {
+
+    const WORDLIST_ERROR_MSG = 'ERROR: Could not write to wordlist!';
+    private $wordlist;
+
+    public function __construct() {
+        if (!file_exists(WORDLIST_FILE)) {
+            if (!file_put_contents(WORDLIST_FILE, json_encode([], JSON_PRETTY_PRINT))) {
+                echo self::WORDLIST_ERROR_MSG;
+                exit(1);
+            }
+        }
+        $this->wordlist = json_decode(file_get_contents(WORDLIST_FILE), true);
+    }
+
+    public function sort_wordlist() {
+        $temp_list = [];
+        foreach ($this->wordlist as $k => $word) {
+            $temp_list[$k] = $word['keyword'] ?? $word;
+        }
+        asort($temp_list);
+        $new_list = [];
+        foreach ($temp_list as $k => $w) {
+            $word = $this->wordlist[$k];
+            if (isset($word['limit'])) {
+                if ($word['limit'] === DEFAULT_LIMIT) {
+                    $word = $word['keyword'];
+                }
+                else {
+                    $word['limit'] = intval($word['limit']);
+                }
+            }
+            $new_list[] = $word;
+        }
+        $this->wordlist = $new_list;
+    }
+
+
+    /**
+     * -------------------------------------------------------------------
+     * Search for Word in wordlist and returns key if found
+     *
+     * @param string $needle Word to search for
+     * @return int|string Key if found otherwise -1
+     */
+    private function find_word(string $needle) {
+        foreach ($this->wordlist as $k => $word) {
+            if ($word === $needle) {
+                return $k;
+            }
+            elseif (isset($word['keyword']) && $word['keyword'] === $needle) {
+                return $k;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * -------------------------------------------------------------------
+     * Builds entry for wordlist
+     *
+     * @param string $word
+     * @param int $limit
+     * @return array|string
+     */
+    private function build_wordlist_entry(string $word, int $limit = 0) {
+        if ($limit === 0 || $limit === DEFAULT_LIMIT) {
+            return $word;
+        }
+        return [
+            'keyword' => $word,
+            'limit'   => $limit
+        ];
+    }
+
+    public function add_or_update(string $word, int $limit = 0): bool {
+        if ($limit > 50) {
+            echo "Max limit is 50!";
+            return false;
+        }
+        $key = $this->find_word($word);
+        if ($key === -1) {
+            return $this->add($word, $limit);
+        }
+        return $this->update($word, $limit, $key);
+    }
+
+    public function remove(string $word): bool {
+        $key = $this->find_word($word);
+        if ($key === -1) {
+            echo "Word '$word' not found in wordlist!" . PHP_EOL;
+            return true;
+        }
+        unset($this->wordlist[$key]);
+        if ($this->save_wordlist()) {
+            echo "Word '$word' removed from wordlist" . PHP_EOL;
+            return true;
+        }
+        echo self::WORDLIST_ERROR_MSG;
+        return false;
+    }
+
+    private function update(string $word, int $limit, $key): bool {
+        if (!$this->is_update_needed($key, $limit)) {
+            echo "No action needed";
+            return true;
+        }
+        $this->wordlist[$key] = $this->build_wordlist_entry($word, $limit);
+        if ($this->save_wordlist()) {
+            echo "Updated limit '$word' in wordlist (new limit: '$limit')" . PHP_EOL;
+            return true;
+        }
+        echo self::WORDLIST_ERROR_MSG;
+        return false;
+    }
+
+    private function add(string $word, int $limit): bool {
+        $this->wordlist[] = $this->build_wordlist_entry($word, $limit);
+        if ($this->save_wordlist()) {
+            echo "Success: Added word '$word' to wordlist";
+            return true;
+        }
+        echo self::WORDLIST_ERROR_MSG;
+        return false;
+    }
+
+    private function is_update_needed($key, $limit): bool {
+        if (isset($this->wordlist[$key]['limit'])) {
+            return intval($this->wordlist[$key]['limit']) !== intval($limit);
+        }
+        return $limit === DEFAULT_LIMIT;
+    }
+
+    private function save_wordlist(): bool {
+        $this->sort_wordlist();
+        return boolval(file_put_contents(
+            WORDLIST_FILE,
+            json_encode($this->wordlist, JSON_PRETTY_PRINT)
+        ));
+    }
+}
