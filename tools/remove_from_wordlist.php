0a80accd9d1be6d7835acf7a2ef735e03f7342a9
Daniel Doving <d.doving@glarotech.ch>
2022-08-02 16:00:34 +0200

    fixed autoload path for wordlist tools

--- tools/remove_from_wordlist.php
+++ tools/remove_from_wordlist.php
@@ -2,7 +2,7 @@
 
 use gif_header\wordlist_manager;
 
-require_once __DIR__ . '/autoload.php';
+require_once dirname(__DIR__) . '/autoload.php';
 
 if (!isset($argv[1])) {
     echo "No word provided" . PHP_EOL;
